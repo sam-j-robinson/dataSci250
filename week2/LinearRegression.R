33a0178626b5ee6709d928c94fd915ead6d5ee16
Sam Robinson <srobinson@ember-entertainment.com>
2017-06-15 15:50:39 -0700

    making sure dataSci250 file is up to date

--- week2/LinearRegression.R
+++ week2/LinearRegression.R
@@ -1,33 +1,3 @@
-# Lab2
-# Simple Linear Regression
-
-## 1 - Create variable sets
-# Response variable
-rate <- c(9.34 ,   8.50  ,  7.62  ,  6.93  ,  6.60)
-
-# Explanatory variable
-year <- c(2000 ,   2001  ,  2002  ,  2003 ,   2004)
-
-# Plot variables
-plot(year,rate
-    ,main="Commercial Banks Interest Rate for 4 Year Car Loan"
-    ,sub="http://www.federalreserve.gov/releases/g19/20050805/"
-  )
-
-# Correlation
-cor(year,rate)
-
-# Linear Model
-fit <- lm(rate ~ year) # (Response ~ Explanatory)
-attributes(fit)
-
-# Plot regression
-plot(year,rate
-     ,main="Commercial Banks Interest Rate for 4 Year Car Loan"
-     ,sub="http://www.federalreserve.gov/releases/g19/20050805/"
-)
-abline(fit) # Plot regression line
-
-# F-Test
-summary(fit)
-
+version https://git-lfs.github.com/spec/v1
+oid sha256:60207a6dbb18e405fbd52d3b1f8d02b58ada2f08c22cc22d81a15cdba7196749
+size 744
